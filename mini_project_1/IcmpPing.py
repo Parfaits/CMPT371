495660d7f71959e1a473fed5b2abc7f8f3b92d1f
Parfaits <36318895+Parfaits@users.noreply.github.com>
2019-08-11 19:11:30 -0700

    Update IcmpPing.py

--- mini_project_1/IcmpPing.py
+++ mini_project_1/IcmpPing.py
@@ -1,5 +1,17 @@
-# The rest are from CMPT371 given by Ouldooz Baghban Karimi
+from socket import *
+import os
+import sys
+import struct
+import time
+import select
+import binascii  
+
+ICMP_ECHO_REQUEST = 8
+
+# ====================== READ ME ======================
+# The rest are given by Ouldooz Baghban Karimi
 # The '# Fill in start' to '# Fill in end' are my part
+# ====================== READ ME ======================
 
 # Fill in start
 # part 2
@@ -15,6 +27,28 @@ errorDict = {3: {0: "Destination network unreachable", 1: "Destination host unre
 			}
 # Fill in end
 
+def checksum(string): 
+	csum = 0
+	countTo = (len(string) // 2) * 2  
+	count = 0
+
+	while count < countTo:
+		thisVal = ord(string[count+1]) * 256 + ord(string[count]) 
+		csum = csum + thisVal 
+		csum = csum & 0xffffffff  
+		count = count + 2
+	
+	if countTo < len(string):
+		csum = csum + ord(string[len(string) - 1])
+		csum = csum & 0xffffffff 
+	
+	csum = (csum >> 16) + (csum & 0xffff)
+	csum = csum + (csum >> 16)
+	answer = ~csum 
+	answer = answer & 0xffff 
+	answer = answer >> 8 | (answer << 8 & 0xff00)
+	return answer 
+	
 def receiveOnePing(mySocket, ID, timeout, destAddr):
 	timeLeft = timeout
 	
@@ -62,6 +96,46 @@ def receiveOnePing(mySocket, ID, timeout, destAddr):
 		if timeLeft <= 0:
 			return "Request timed out."
 
+	
+def sendOnePing(mySocket, destAddr, ID):
+	# Header is type (8), code (8), checksum (16), id (16), sequence (16)
+	
+	myChecksum = 0
+	# Make a dummy header with a 0 checksum
+	# struct -- Interpret strings as packed binary data
+	header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, ID, 1)
+	data = struct.pack("d", time.time())
+	# Calculate the checksum on the data and the dummy header.
+
+	myChecksum = checksum(str(header + data)) 
+	
+	# Get the right checksum, and put in the header
+	if sys.platform == 'darwin':
+		# Convert 16-bit integers from host to network byte order
+		myChecksum = htons(myChecksum) & 0xffff		
+	else:
+		myChecksum = htons(myChecksum)
+		
+	header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, ID, 1)
+	packet = header + data
+	
+	mySocket.sendto(packet, (destAddr, 1)) # AF_INET address must be tuple, not str
+	# Both LISTS and TUPLES consist of a number of objects
+	# which can be referenced by their position number within the object.
+	
+def doOnePing(destAddr, timeout): 
+	icmp = getprotobyname("icmp")
+
+	# SOCK_RAW is a powerful socket type. For more details:   http://sock-raw.org/papers/sock_raw
+	mySocket = socket(AF_INET, SOCK_RAW, icmp)
+	
+	myID = os.getpid() & 0xFFFF  # Return the current process i
+	sendOnePing(mySocket, destAddr, myID)
+	delay = receiveOnePing(mySocket, myID, timeout, destAddr)
+	
+	mySocket.close()
+	return delay
+	
 def ping(host, timeout=1):
 	# timeout=1 means: If one second goes by without a reply from the server,
 	# the client assumes that either the client's ping or the server's pong is lost
@@ -107,3 +181,12 @@ def ping(host, timeout=1):
 		sys.exit()
 		# Fill in end
 	# Fill in end
+
+# Bunch of ip addresses to test
+# ping("google.com")
+# ping("127.0.0.1")
+# ping("192.168.1.134")
+# ping("131.255.7.26") # South America: Buenos Aires (Argentina)
+# ping("195.201.213.247") # Europe: Frankfurt (Germany)
+# ping("110.50.243.6") # Asia: Tokyo (Japan)
+ping("101.0.86.43") # Australia: Sydney
